5c90e30d7dca54e2344d9191f731c8b468010fca
Daniel Moraes <daniel.b.moraes@gmail.com>
2017-05-21 17:17:20 -0300

    Add tests for auth reducer.

--- app/redux/tests/reducers.test.js
+++ app/redux/tests/reducers.test.js
@@ -1,6 +1,7 @@
 import expect from 'expect'
 
-import reducer from '../reducers/app'
+import appReducer from '../reducers/app'
+import authReducer from '../reducers/auth'
 import * as types from '../actionTypes'
 
 describe('app reducer', () => {
@@ -13,14 +14,14 @@ describe('app reducer', () => {
 
   it('should return the initial state', () => {
     expect(
-      reducer(undefined, {})
+      appReducer(undefined, {})
     ).toEqual(initialState)
   })
 
   it('should handle FETCHING', () => {
     const state = { ...initialState, pendingRequests: 5 }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.FETCHING
       })
     ).toEqual({ ...state, pendingRequests: 6 })
@@ -29,7 +30,7 @@ describe('app reducer', () => {
   it('should handle FETCHING_ENDED', () => {
     const state = { ...initialState, pendingRequests: 5 }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.FETCHING_ENDED
       })
     ).toEqual({ ...state, pendingRequests: 4 })
@@ -39,7 +40,7 @@ describe('app reducer', () => {
     const state = { ...initialState, isLoaded: true, showLoadingOverlay: false }
 
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.LOADING_AUTH_USER
       })
     ).toEqual({ ...state, isLoaded: false, showLoadingOverlay: true })
@@ -48,7 +49,7 @@ describe('app reducer', () => {
   it('should handle LOADING_AUTH_USER_SUCCEEDED', () => {
     const state = { ...initialState, isLoaded: false, showLoadingOverlay: true }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.LOADING_AUTH_USER_SUCCEEDED
       })
     ).toEqual({ ...state, isLoaded: true, showLoadingOverlay: false })
@@ -57,7 +58,7 @@ describe('app reducer', () => {
   it('should handle LOADING_AUTH_USER_FAILED', () => {
     const state = { ...initialState, isLoaded: false, showLoadingOverlay: true }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.LOADING_AUTH_USER_FAILED
       })
     ).toEqual({ ...state, isLoaded: true, showLoadingOverlay: false })
@@ -66,7 +67,7 @@ describe('app reducer', () => {
   it('should handle SIGN_IN_CANCEL', () => {
     const state = { ...initialState, signInFailed: true }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGN_IN_CANCEL
       })
     ).toEqual({ ...state, signInFailed: false })
@@ -80,7 +81,7 @@ describe('app reducer', () => {
     }
 
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGNING_IN
       })
     ).toEqual({ ...state, signInFailed: false, showLoadingOverlay: true })
@@ -94,7 +95,7 @@ describe('app reducer', () => {
     }
 
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGNING_IN_SUCCEEDED
       })
     ).toEqual({ ...state, signInFailed: false, showLoadingOverlay: false })
@@ -108,7 +109,7 @@ describe('app reducer', () => {
     }
 
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGNING_IN_FAILED
       })
     ).toEqual({ ...state, signInFailed: true, showLoadingOverlay: false })
@@ -117,7 +118,7 @@ describe('app reducer', () => {
   it('should handle SIGNING_OUT', () => {
     const state = { ...initialState, showLoadingOverlay: false }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGNING_OUT
       })
     ).toEqual({ ...state, showLoadingOverlay: true })
@@ -126,7 +127,7 @@ describe('app reducer', () => {
   it('should handle SIGNING_OUT_SUCCEEDED', () => {
     const state = { ...initialState, showLoadingOverlay: true }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGNING_OUT_SUCCEEDED
       })
     ).toEqual({ ...state, showLoadingOverlay: false })
@@ -135,9 +136,71 @@ describe('app reducer', () => {
   it('should handle SIGNING_OUT_FAILED', () => {
     const state = { ...initialState, showLoadingOverlay: true }
     expect(
-      reducer(state, {
+      appReducer(state, {
         type: types.SIGNING_OUT_FAILED
       })
     ).toEqual({ ...state, showLoadingOverlay: false })
+  })
+})
+
+describe('auth reducer', () => {
+  const initialState = {
+    user: {},
+    isSigningOut: false
+  }
+
+  const user = { id: 0, name: 'user', email: 'user@user.com' }
+
+  it('should return the initial state', () => {
+    expect(
+      authReducer(undefined, {})
+    ).toEqual(initialState)
+  })
+
+  it('should handle SIGNING_IN_SUCCEEDED', () => {
+    const state = { ...initialState, user: {} }
+    expect(
+      authReducer(state, {
+        type: types.SIGNING_IN_SUCCEEDED,
+        payload: user
+      })
+    ).toEqual({ ...state, user })
+  })
+
+  it('should handle LOADING_AUTH_USER_SUCCEEDED', () => {
+    const state = { ...initialState, user: {} }
+    expect(
+      authReducer(state, {
+        type: types.LOADING_AUTH_USER_SUCCEEDED,
+        payload: user
+      })
+    ).toEqual({ ...state, user })
+  })
+
+  it('should handle SIGNING_OUT', () => {
+    const state = { ...initialState, isSigningOut: false }
+    expect(
+      authReducer(state, {
+        type: types.SIGNING_OUT
+      })
+    ).toEqual({ ...state, isSigningOut: true })
+  })
+
+  it('should handle SIGNING_OUT_SUCCEEDED', () => {
+    const state = { ...initialState, isSigningOut: true, user }
+    expect(
+      authReducer(state, {
+        type: types.SIGNING_OUT_SUCCEEDED
+      })
+    ).toEqual({ ...state, isSigningOut: false, user: {} })
+  })
+
+  it('should handle SIGNING_OUT_FAILED', () => {
+    const state = { ...initialState, isSigningOut: true, user }
+    expect(
+      authReducer(state, {
+        type: types.SIGNING_OUT_FAILED
+      })
+    ).toEqual({ ...state, isSigningOut: false, user })
   })
 })
